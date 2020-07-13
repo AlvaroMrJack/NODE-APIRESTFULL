41fbf795455673ab024961618e0e1bb6b49f42f7
AlvaroMrJack <alvaro.12tobar@gmail.com>
2020-07-13 19:38:50 -0400

    Instalación de nodemon y configuración de servidor web

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+//para configurar el puerto
+app.set("port", process.env.PORT || 3000);
+
+//para aceptar formatos json (midleware)
+app.use(express.json());
+
+app.use("/", (req, res)=>{
+	res.json(
+		{message: "Hola"}
+	);
+});
+
+//para configurar por donde escucha el servidor
+app.listen(app.get("port"), ()=>{
+	console.log("Servidor corriendo en el puerto: ", app.get("port"));
+});
