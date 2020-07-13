3f9dfbf738680102469237eff769bd27f5e1e254
AlvaroMrJack <alvaro.12tobar@gmail.com>
2020-07-13 19:26:07 -0400

    Comienzo del proyecto

---
+++ db/context.js
@@ -0,0 +1,24 @@
+module.exports = userData = [{
+	id: 1,
+	userName: "MrJack",
+	name: "Alvaro",
+	email: "alvaro.12tobar@gmail.com",
+	password: "1234",
+	createDate: new Date(2020, 05, 18)
+},
+{
+	id: 1,
+	userName: "MrJack",
+	name: "Alvaro",
+	email: "alvaro.12tobar@gmail.com",
+	password: "1234",
+	createDate: new Date(2020, 05, 18)
+},
+{
+	id: 1,
+	userName: "MrJack",
+	name: "Alvaro",
+	email: "alvaro.12tobar@gmail.com",
+	password: "1234",
+	createDate: new Date(2020, 05, 18)
+}];
